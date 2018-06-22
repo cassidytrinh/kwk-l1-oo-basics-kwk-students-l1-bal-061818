cd16eba289245e795e18d4e8ceaa2a6d10a58f70
cassidytrinh <cassidytrinh@gmail.com>
2018-06-22 18:24:36 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,12 @@
 
 class Shoe
-  attr_accessor :brand, :color, :size
-  attr_reader :title
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 
   def initialize(brand)
     @brand = brand
   end
 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
+  def 
 
 end
